88464ec66bcfd291911e28490033d9537d0b4cbe
MarcinKwiatkowski04 <46976228+MarcinKwiatkowski04@users.noreply.github.com>
2021-04-04 16:41:43 +0200

    zmianki

---
+++ ProducentKonsumentWPF/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System.Windows;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace ProducentKonsumentWPF
+{
+    /// <summary>
+    /// interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string connectionString = "Data Source=LAPTOP-564H2LL3;Initial Catalog=PK;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        
+
+        // public static Label prodlabel1;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            ClearParametersTable();
+            
+        }
+
+        public void ClearParametersTable()
+        {
+            connection.Open();
+
+            string queryRemoveParams = "DELETE FROM Parameters";
+            SqlCommand cmdRemoveParams = new SqlCommand(queryRemoveParams, connection);
+            cmdRemoveParams.ExecuteScalar();
+
+            connection.Close();
+        }
+
+        public void FillParameters()
+        {
+            connection.Open();           
+            
+
+            if (inputModeComboBox.SelectedIndex == 0)
+            {
+                string queryAddParams = "INSERT INTO Parameters(StorageSize, ProgramMode) VALUES (" + Int32.Parse(inputStorageSizeTextBox.Text) + ", 0)";
+                SqlCommand cmdAddParams = new SqlCommand(queryAddParams, connection);
+                cmdAddParams.ExecuteScalar();
+            }
+            else if (inputModeComboBox.SelectedIndex == 1)
+            {
+                string queryAddParams = "INSERT INTO Parameters(StorageSize, ProgramMode) VALUES (" + Int32.Parse(inputStorageSizeTextBox.Text) + ", 1)";
+                SqlCommand cmdAddParams = new SqlCommand(queryAddParams, connection);
+                cmdAddParams.ExecuteScalar();
+            }
+            else if (inputModeComboBox.SelectedIndex == 2)
+            {
+                string queryAddParams = "INSERT INTO Parameters(StorageSize, ProgramMode) VALUES (" + Int32.Parse(inputStorageSizeTextBox.Text) + ", 2)";
+                SqlCommand cmdAddParams = new SqlCommand(queryAddParams, connection);
+                cmdAddParams.ExecuteScalar();
+            }
+            else if (inputModeComboBox.SelectedIndex == 3)
+            {
+                string queryAddParams = "INSERT INTO Parameters(StorageSize, ProgramMode) VALUES (" + Int32.Parse(inputStorageSizeTextBox.Text) + ", 3)";
+                SqlCommand cmdAddParams = new SqlCommand(queryAddParams, connection);
+                cmdAddParams.ExecuteScalar();
+            }
+
+            connection.Close();
+        }
+
+        private void inputStorageSizeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int parsedValue;
+            if (!int.TryParse(inputStorageSizeTextBox.Text, out parsedValue))
+            {
+                MessageBox.Show("This is a number only field");
+                return;
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            FillParameters();
+            x1p1kWindow x1p1kWin = new x1p1kWindow();
+            JedenProdWieluKons JpWk = new JedenProdWieluKons();
+            WieluProd1Kons WpJk = new WieluProd1Kons();
+            WieluProdWieluKons WpWk = new WieluProdWieluKons();
+
+            if (inputModeComboBox.SelectedIndex == 0) x1p1kWin.Show();
+            else if (inputModeComboBox.SelectedIndex == 1) WpJk.Show();
+            else if (inputModeComboBox.SelectedIndex == 2) JpWk.Show();
+            else if (inputModeComboBox.SelectedIndex == 3) WpWk.Show();
+            this.Close();
+        }
+    }
+        
+}
---
+++ ProducentKonsumentWPF/WieluProd1Kons.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProducentKonsumentWPF
+{
+    /// <summary>
+    /// Interaction logic for WieluProd1Kons.xaml
+    /// </summary>
+    public partial class WieluProd1Kons : Window
+    {
+        static string connectionString = "Data Source=LAPTOP-564H2LL3;Initial Catalog=PK;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        public static Label prodlabel1;
+        public static int storageSize;
+
+
+        public WieluProd1Kons()
+        {
+            InitializeComponent();
+            UruchomWątki();
+
+        }
+
+
+        Random rand = new Random();
+        static BlockingCollection<int> Buffer = new BlockingCollection<int>();
+        public int BufferSize = storageSize;
+        static int BufferEmptyPlaces = storageSize;
+
+
+        int produkt = 0;
+        public void Produce(BlockingCollection<int> buffer, int nrProd)
+        {
+
+            
+            int index = 0;
+            bool canProduce = false;
+            while (true)
+            {
+                if (connection.State != ConnectionState.Open) connection.Open();
+
+                if (BufferEmptyPlaces == 0 || BufferEmptyPlaces == storageSize)
+                {
+
+                    canProduce = false;
+                    while (!canProduce)
+                    {
+
+                        Thread.Sleep(10);
+                        if (BufferEmptyPlaces > 0)
+                        {
+                            canProduce = true;
+                        }
+                    }
+                }
+
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    producerListbox.Items.Add("Producent " + nrProd + " wyprodukował produkt nr " + produkt);
+                    producerListbox.SelectedIndex = producerListbox.Items.Count - 1;
+                    producerListbox.ScrollIntoView(producerListbox.SelectedItem);
+                }));
+                Thread.Sleep(rand.Next(1000));
+
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    string queryAddItem = "INSERT INTO Magazyn (Produkt) VALUES ( " + produkt + ")";
+                    SqlCommand cmdAddItem = new SqlCommand(queryAddItem, connection);
+                    cmdAddItem.ExecuteScalar();
+                }));
+
+
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    string queryGetItem = "SELECT Produkt FROM Magazyn where Produkt = " + produkt;
+                    SqlCommand cmdGetItem = new SqlCommand(queryGetItem, connection);
+                    produkt = Int32.Parse(cmdGetItem.ExecuteScalar().ToString());
+                    warehouseListbox.Items.Add(produkt);
+                }));
+
+
+                Thread.Sleep(10);
+                buffer.Add(produkt);
+                BufferEmptyPlaces--;
+                produkt++;
+                index++;
+
+                connection.Close();
+            }
+
+        }
+
+        public void Consume(BlockingCollection<int> buffer)
+        {
+
+
+            int index = 0;
+            while (true)
+            {
+                // connection.Open();
+
+                foreach (var i in buffer.GetConsumingEnumerable())
+                {
+
+                    Dispatcher.Invoke(new Action(() =>
+                    {
+                        consumerListbox.Items.Add("Konsumuję produkt nr " + i);
+                        consumerListbox.SelectedIndex = consumerListbox.Items.Count - 1;
+                        consumerListbox.ScrollIntoView(consumerListbox.SelectedItem);
+                        warehouseListbox.Items.Remove(i);
+                    }));
+
+                    Dispatcher.Invoke(new Action(() =>
+                    {
+                        string queryRemoveItem = "DELETE FROM Magazyn WHERE Produkt = " + i;
+                        SqlCommand cmdRemoveItem = new SqlCommand(queryRemoveItem, connection);
+                        cmdRemoveItem.ExecuteScalar();
+                    }));
+
+                    Thread.Sleep(rand.Next(2000));
+
+                    Thread.Sleep(20);
+                    index++;
+                    BufferEmptyPlaces++;
+                }
+
+                // connection.Close();
+            }
+
+        }
+        public Task UruchomWątki()
+        {
+            connection.Open();
+
+            string queryRemoveItems = "DELETE FROM Magazyn";
+            SqlCommand cmdRemoveItems = new SqlCommand(queryRemoveItems, connection);
+            cmdRemoveItems.ExecuteScalar();
+
+            string queryGetStorageSize = "SELECT StorageSize FROM Parameters";
+            SqlCommand cmdGetStorageSize = new SqlCommand(queryGetStorageSize, connection);
+            storageSize = Int32.Parse(cmdGetStorageSize.ExecuteScalar().ToString());
+
+
+            connection.Close();
+            BufferSize = storageSize;
+            BufferEmptyPlaces = storageSize;
+
+            var producerTask = Task.Run(() => Produce(Buffer, 1));
+            var producerTask1 = Task.Run(() => Produce(Buffer, 2));
+            var producerTask2 = Task.Run(() => Produce(Buffer, 3));
+            var consumeTask = Task.Run(() => Consume(Buffer));
+
+
+
+            return null;
+        }
+    }
+}
